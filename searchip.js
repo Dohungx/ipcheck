aefb474a342e13046cad455217b6a165ccdb4094
Dohungx <163644148+Dohungx@users.noreply.github.com>
2024-03-25 23:57:17 +0700

    Update searchip.js

--- searchip.js
+++ searchip.js
@@ -1,25 +1,23 @@
-// Hàm tìm kiếm IP
-async function searchIP() {
-    const ipAddress = document.getElementById('search-ip').value;
-    try {
-        const response = await fetch(`https://ipinfo.io/${ipAddress}/json`);
-        const ipInfo = await response.json();
-        // Hiển thị thông tin liên quan đến địa chỉ IP
-        displayIPInfo(ipInfo);
-    } catch (error) {
-        console.error('Lỗi khi tìm kiếm địa chỉ IP:', error);
-        // Hiển thị thông báo lỗi nếu có lỗi xảy ra
-        document.getElementById('ip-info').innerText = 'Không tìm thấy thông tin cho địa chỉ IP này.';
-    }
-}
+<script>
+    function searchIP() {
+        var ip = document.getElementById("search-ip").value;
+        var url = "https://api.ipgeolocation.io/ipgeo?apiKey=YOUR_API_KEY&ip=" + ip;
 
-// Hàm hiển thị thông tin liên quan đến địa chỉ IP
-function displayIPInfo(ipInfo) {
-    const ipInfoHTML = `
-        <p>Địa chỉ IP: ${ipInfo.ip}</p>
-        <p>Vị trí: ${ipInfo.city}, ${ipInfo.region}, ${ipInfo.country}</p>
-        <p>Nhà cung cấp dịch vụ internet: ${ipInfo.org || 'Không rõ'}</p>
-    `;
-    // Hiển thị thông tin trong khu vực có id="ip-info"
-    document.getElementById('ip-info').innerHTML = ipInfoHTML;
-}
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                // Xử lý dữ liệu trả về ở đây
+                var ipInfo = document.getElementById("ip-info");
+                ipInfo.innerHTML = "Quốc gia: " + data.country_name + "<br>" +
+                                    "Tỉnh/Thành phố: " + data.city + "<br>" +
+                                    "Khu vực: " + data.region + "<br>" +
+                                    "Mã bưu chính: " + data.postal + "<br>" +
+                                    "Tọa độ: " + data.latitude + ", " + data.longitude;
+            })
+            .catch(error => {
+                console.log(error);
+                var ipInfo = document.getElementById("ip-info");
+                ipInfo.innerHTML = "Không tìm thấy thông tin cho địa chỉ IP này.";
+            });
+    }
+</script>
