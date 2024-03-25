369854b93fde18ed04404cee028580fc133e4837
Dohungx <163644148+Dohungx@users.noreply.github.com>
2024-03-26 00:00:36 +0700

    Update searchip.js

--- searchip.js
+++ searchip.js
@@ -1,23 +1,18 @@
 // searchip.js
 
 function searchIP() {
-    var ip = document.getElementById("search-ip").value;
-    var url = "https://api.ipgeolocation.io/ipgeo?apiKey=YOUR_API_KEY&ip=" + ip;
-
-    fetch(url)
+    fetch("https://ipinfo.io/json")
         .then(response => response.json())
         .then(data => {
-            // Xử lý dữ liệu trả về ở đây
             var ipInfo = document.getElementById("ip-info");
-            ipInfo.innerHTML = "Quốc gia: " + data.country_name + "<br>" +
-                                "Tỉnh/Thành phố: " + data.city + "<br>" +
+            ipInfo.innerHTML = "IP: " + data.ip + "<br>" +
+                                "Quốc gia: " + data.country + "<br>" +
                                 "Khu vực: " + data.region + "<br>" +
+                                "Thành phố: " + data.city + "<br>" +
                                 "Mã bưu chính: " + data.postal + "<br>" +
-                                "Tọa độ: " + data.latitude + ", " + data.longitude;
+                                "Tọa độ: " + data.loc;
         })
         .catch(error => {
-            console.log(error);
-            var ipInfo = document.getElementById("ip-info");
-            ipInfo.innerHTML = "Không tìm thấy thông tin cho địa chỉ IP này.";
+            console.error("Đã xảy ra lỗi: " + error);
         });
 }
